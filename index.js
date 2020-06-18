2bd833c32918d5b746db8632ce9815b046710f14
峡州仙士 <c@cjh0613.com>
2020-06-18 14:55:35 +0800

    edit 3 deployer.register

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 hexo.extend.generator.register('cjh_url_generator', require('./lib/generator'));
-hexo.extend.deployer.register('cjh_url_submitter', require('./lib/url_submitter'));
+hexo.extend.deployer.register('cjh_url_submitter', require('./lib/baidu_submitter'));
+hexo.extend.deployer.register('cjh_bing_url_submitter', require('./lib/bing_submitter'));
+hexo.extend.deployer.register('cjh_google_url_submitter', require('./lib/google_submitter'));
